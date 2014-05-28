212db02fdb005ff03e9cf916fb63b6269e55cfdd
Bill Dami <billdami@gmail.com>
2014-05-28 14:45:36 -0400

    progress commit

--- example/example.js
+++ example/example.js
@@ -2,5 +2,13 @@ var App = Ember.Application.create({});
 
 App.ApplicationController = Ember.ObjectController.extend({
     items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    spinboxValue: 3
+    spinboxValue: 3,
+    //items: ['AM', 'PM'],
+    //spinboxValue: 'AM',
+
+    actions: {
+        spinboxUpdate: function(value, index) {
+            //console.log('spinbox updated (new value: ' + value + ', new index: ' + index + ')');
+        }
+    }
 });

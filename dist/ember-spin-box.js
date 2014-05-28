cf53c52ce7111907a5fb611b7caf02668d34e357
Bill Dami <billdami@gmail.com>
2014-05-28 09:42:10 -0400

    progress commit

--- dist/ember-spin-box.js
+++ dist/ember-spin-box.js
@@ -45,6 +45,16 @@ App.SpinBoxItemsView = Ember.ContainerView.extend({
         });
     },
 
+    disableTransitions: function() {
+        this.$el.css({
+            '-webkit-transition': '',
+            '-moz-transition': '',
+            '-ms-transition': '',
+            '-o-transition': '',
+            'transition': ''
+        });
+    },
+
     handleOffsetChange: function() {
         var cssVal = 'translateY(' + this.get('yOffset') + 'px)';
         this.$el.css({
@@ -84,11 +94,13 @@ App.SpinBoxComponent = Ember.Component.extend({
     finishDelay: 300,
 
     _totalSpinOffset: 0,
+    _betweenRowOffset: 0,
 
     setup: function() {
+        var initIndex = this.indexOf(this.get('value'));
         this.$el = this.$();
         this.adjustHeight();
-        this.set('_selectedIndex', this.indexOf(this.get('value')));
+        this.set('_selectedIndex', initIndex);
         this.$el.on('mousewheel DOMMouseScroll', Em.run.bind(this, this.handleMouseWheel));
     }.on('didInsertElement'),
 
@@ -117,7 +129,7 @@ App.SpinBoxComponent = Ember.Component.extend({
 
         for(var i=0; i<numRows; i++) {
             views.push(rowView.create({
-                index: this.adjustedIndex(startIndex + i),
+                index: startIndex + i,
                 value: this.valueAt(startIndex + i),
                 top: rowH * i
             }));
@@ -134,6 +146,10 @@ App.SpinBoxComponent = Ember.Component.extend({
         var index = this.get('content').indexOf(value);
         return index === -1 ? 0 : index;
     },
+
+    selectedIndex: function() {
+        return this.adjustedIndex(this.get('_selectedIndex'));
+    }.property('_selectedIndex'),
 
     contentLength: function() {
         return this.get('content').length;
@@ -152,39 +168,42 @@ App.SpinBoxComponent = Ember.Component.extend({
         return index;
     },
 
-    spin: function(direction, distance) {
+    spin: function(yOffset) {
             var ct = this.get('itemsView'),
             rowH = this.get('rowHeight'),
             selIndex = this.get('_selectedIndex'),
+            betweenRowOffset = this.get('_betweenRowOffset') + yOffset,
+            totalOffset = this.get('_totalSpinOffset') + yOffset,
             child,
             prevChild,
-            newIndex,
-            yOffset;
+            newIndex;
 
         Em.run.cancel(this.get('_finishSpinTimer'));
 
-        if(direction === 'up') {
-            yOffset = distance;
-            prevChild = ct.objectAt(0);
-            newIndex = prevChild.get('index') - 1;
-            child = ct.popObject();
-            ct.unshiftObject(child);
-            child.setProperties({
-                top: prevChild.get('top') - rowH,
-                index: this.adjustedIndex(newIndex),
-                value: this.valueAt(newIndex)
-            });
-        } else {
-            yOffset = distance * -1;
-            prevChild = ct.objectAt(ct.get('childViews').length - 1);
-            newIndex = prevChild.get('index') + 1;
-            child = ct.shiftObject();
-            ct.pushObject(child);
-            child.setProperties({
-                top: prevChild.get('top') + rowH,
-                index: this.adjustedIndex(newIndex),
-                value: this.valueAt(newIndex)
-            });
+        if(Math.abs(betweenRowOffset) >= rowH) {
+            betweenRowOffset = 0;
+
+            if(yOffset > 0) {
+                prevChild = ct.objectAt(0);
+                newIndex = prevChild.get('index') - 1;
+                child = ct.popObject();
+                ct.unshiftObject(child);
+                child.setProperties({
+                    top: prevChild.get('top') - rowH,
+                    index: newIndex,
+                    value: this.valueAt(newIndex)
+                });
+            } else {
+                prevChild = ct.objectAt(ct.get('childViews').length - 1);
+                newIndex = prevChild.get('index') + 1;
+                child = ct.shiftObject();
+                ct.pushObject(child);
+                child.setProperties({
+                    top: prevChild.get('top') + rowH,
+                    index: newIndex,
+                    value: this.valueAt(newIndex)
+                });
+            }
         }
         
         ct.adjustYOffset(yOffset);
@@ -192,56 +211,99 @@ App.SpinBoxComponent = Ember.Component.extend({
         this.setProperties({
             _prevSelectedIndex: selIndex !== null ? selIndex : this.get('_prevSelectedIndex'),
             _selectedIndex: null,
-            _totalSpinOffset: this.get('_totalSpinOffset') + yOffset,
+            _totalSpinOffset: totalOffset,
+            _betweenRowOffset: betweenRowOffset,
             _finishSpinTimer: Em.run.later(this, this.finishSpin, this.get('finishDelay'))
         });
     },
 
     finishSpin: function() {
+        if(this.get('_disableFinishSpin')) return;
+
         var offset = this.get('_totalSpinOffset'),
             rowH = this.get('rowHeight'),
             overScroll = Math.abs(offset % rowH),
             prevIndex = this.get('_prevSelectedIndex'),
-            newIndex = prevIndex + ((offset / rowH) * -1);
+            newIndex,
+            newValue;
 
-        if(offset === 0) {
-            return;
-        } else if(overScroll > 0) {
-            return this.spin(offset < 0 ? 'down' : 'up', overScroll < (rowH / 2) ? overScroll : (rowH - overScroll));
+        if(overScroll > 0) {
+            if(overScroll < (rowH / 2)) {
+                return this.spin(offset < 0 ? overScroll : -overScroll);
+            } else {
+                return this.spin(offset < 0 ? -(rowH - overScroll) : rowH - overScroll);
+            }
         }
 
+        newIndex = prevIndex + ((offset / rowH) * -1);
+        newValue = this.valueAt(newIndex);
+
         this.setProperties({
-            _selectedIndex: this.adjustedIndex(newIndex),
+            _selectedIndex: newIndex,
             _totalSpinOffset: 0,
-            value: this.valueAt(newIndex)
+            _betweenRowOffset: 0,
+            value: newValue
         });
+
+        if(offset !== 0) {
+            //send the action after all bindings have been synced
+            Em.run.scheduleOnce('sync', this, function() {
+                this.sendAction('onUpdate', newValue, this.adjustedIndex(newIndex));
+            });
+        }
     },
 
     handleTouchStart: function(e) {
+        this.setProperties({
+            _touchStartTime: e.timeStamp,
+            _touchStartY: e.originalEvent.touches[0].pageY,
+            _disableFinishSpin: true
+        });
+
+        //@todo stop in-progress momentum phase if needed
+
+        this.get('itemsView').disableTransitions();
         e.preventDefault();
-        
     }.on('touchStart'),
 
     handleTouchMove: function(e) {
-        e.preventDefault();
+        var pageY = e.originalEvent.touches[0].pageY,
+            prevY = this.get('_touchMoveY') ? this.get('_touchMoveY') : this.get('_touchStartY');
+            offset = pageY - prevY;
+        
+        this.setProperties({
+            _touchMoveY: pageY,
+            _touchMoveTime: e.timeStamp
+        });
 
+        this.spin(offset);
+        e.preventDefault();
     }.on('touchMove'),
 
     handleTouchEnd: function(e) {
+        this.setProperties({
+            _touchEndTime: e.timeStamp,
+            _touchMoveY: null,
+            _disableFinishSpin: false
+        });
+
+        //@todo momentum scrolling phase (try to use CSS-based custom easing function when applying `transition` properties)
+
+        this.get('itemsView').enableTransitions();
+        this.finishSpin();
         e.preventDefault();
-        
     }.on('touchEnd'),
 
     handleKeyDown: function(e) {
         if(e.keyCode != 38 && e.keyCode != 40) return;
         e.preventDefault();
-        this.spin(e.keyCode == 38 ? 'up' : 'down', this.get('rowHeight'));
+        this.spin((e.keyCode == 38 ? 1 : -1) * this.get('rowHeight'));
     }.on('keyDown'),
 
     handleMouseWheel: function(e) {
         var origEvent = e.originalEvent;
         e.preventDefault();
-        this.spin((origEvent.wheelDelta > 0 || origEvent.detail < 0) ? 'up' : 'down', this.get('rowHeight'));
+        this.spin(((origEvent.wheelDelta > 0 || origEvent.detail < 0) ? 1 : -1) * this.get('rowHeight'));
     },
 
     registerItemsView: function(view) {
